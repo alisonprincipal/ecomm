ad853938989ff028e29bc464b53e1710a6730bd7
Gabriel Gabriel Alison Junho <n134401@gruposantander.com>
2023-06-01 14:48:10 -0300

    fix: corrigido erro no tipo de dados dos produtos

--- mongo/insereProdutos.js
+++ mongo/insereProdutos.js
@@ -1,5 +1,11 @@
 const produtos = require("./produtos.json")
 use("ecomm");
+const numeroDecimal = produtos.map((element=>{
+    if(element.preco){
+        element.preco = NumberDecimal(element.preco.toFixed(2))
+    }
+    return element
+}))
 
-const inserindoProdutos = db.products.insertMany(produtos)
+const inserindoProdutos = db.products.insertMany(numeroDecimal)
 console.log(inserindoProdutos)
